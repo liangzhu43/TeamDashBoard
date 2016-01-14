b8904f280a9ab13aedcbe08faea6c4b54e116717
Stanly Lau <stanly@odd-e.com>
2016-01-14 15:38:06 +0800

    Passed the cucumber test

--- features/satisfactions.feature
+++ features/satisfactions.feature
@@ -2,5 +2,5 @@ Feature: Manage Satisfactions
 
   Scenario: Add Satisfaction
     Given there is no satisfaction with name "John Doe"
-    When I add a satisfaction with name "John Doe", category "emotion" and score "5"
+    When I add a satisfaction with name "John Doe", category "emotion", date "2/2/2016" and score "5"
     Then I should see "John Doe" listed
--- features/step_definitions/satisfactions_steps.rb
+++ features/step_definitions/satisfactions_steps.rb
@@ -1,12 +1,18 @@
 Given(/^there is no satisfaction with name "(.*?)"$/) do |name|
   visit '/satisfactions'
-  #expect(page).to.not have_content name
+  expect(page).to_not have_content name
 end
 
-When(/^I add a satisfaction with name "(.*?)", category "(.*?)" and score "(.*?)"$/) do |arg1, arg2, arg3|
-  pending # express the regexp above with the code you wish you had
+When(/^I add a satisfaction with name "(.*?)", category "(.*?)", date "(.*?)" and score "(.*?)"$/) do |name, category, date, score|
+  visit '/satisfactions/new'
+  fill_in('satisfaction_date',:with => date)
+  fill_in('satisfaction_name',:with => name)
+  fill_in('satisfaction_category',:with => category)
+  fill_in('satisfaction_score',:with => score)
+  click_button 'Create Satisfaction'
 end
 
-Then(/^I should see "(.*?)" listed$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then(/^I should see "(.*?)" listed$/) do |name|
+  visit '/satisfactions'
+  expect(page).to have_content name
 end
