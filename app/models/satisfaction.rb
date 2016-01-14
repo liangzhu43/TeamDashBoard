1f12b7aab2dcf8048113e7b3609d18336d670a17
liangzhu43 <liangzhu@titansoft.com.sg>
2016-01-14 16:05:07 +0800

    use date input and add score validation 1-10 #2

--- app/models/satisfaction.rb
+++ app/models/satisfaction.rb
@@ -1,3 +1,4 @@
 class Satisfaction < ActiveRecord::Base
   validates :date,:name,:category,:score, presence: true
+  validates :score, numericality: { only_integer: true, less_than_or_equal_to: 10, greater_than: 0 }
 end
