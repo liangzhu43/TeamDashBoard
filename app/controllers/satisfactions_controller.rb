a3b7bf46b4a83be9f0e5040e61a5642eb030a11e
liangzhu43 <liangzhu@titansoft.com.sg>
2016-01-14 14:50:57 +0800

    create/list new Statisfactions

--- app/controllers/satisfactions_controller.rb
+++ app/controllers/satisfactions_controller.rb
@@ -1,4 +1,28 @@
 class SatisfactionsController < ApplicationController
-    def new
+  def index
+    @satisfactions = Satisfaction.all
+  end
+
+  def show
+    @satisfaction = Satisfaction.find(params[:id])
+  end
+
+  def new
+    @satisfaction = Satisfaction.new
+  end
+
+  def create
+    @satisfaction = Satisfaction.new(satisfaction_params)
+
+    if @satisfaction.save
+      redirect_to @satisfaction
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def satisfaction_params
+      params.require(:satisfaction).permit(:date, :name, :category, :score)
     end
 end
